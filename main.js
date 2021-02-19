01fc49f9215bb0322eb0ee96587520d007fdc531
Jacob Collins <jacob.edward.collins@gmail.com>
2021-02-19 13:50:57 -0500

    Final updates

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
-setInterval(displayTime, 1000);
+function updateClock(){
+    let dateTime = new Date();
+    let hours = dateTime.getHours();
+    let minutes = dateTime.getMinutes();
+    let seconds = dateTime.getSeconds();
+    let am_or_pm = document.getElementById("am_or_pm");
 
-function displayTime() {
-    let date = new Date() 
-    let hour = date.getHours();
-    let mins = date.getMinutes();
-    let secs = date.getSeconds();
-    let period = "AM";
-
-    if (hour>12) {
-        hour -= 12;
-        period = "PM";
+    if(hours >=12){
+        am_or_pm.innerHTML = 'PM';
+    }else{
+        am_or_pm.innerHTML + 'AM';
     }
+    if(hours > 12){
+    hours = hours - 12;
+    
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-    if (hour < 12) {
-        period = "AM";
-    }
-
-hour = hour <10 ? "0" + hour : hour;
-mins = mins < 10 ? "0" + mins : mins;
-secs = secs < 10 ? "0" + secs : secs;
-
-let currentTime = hour + ":" + mins + ":" + secs + period
-
-document.getElementById("JacobsDigitalClock")
-    .innerHTML = currentTime
 }
 
-displayTime()
+document.getElementById('hours').innerHTML = hours;
+document.getElementById('minutes').innerHTML = minutes;
+document.getElementById('seconds').innerHTML = seconds;
+
+}  
+setInterval(updateClock, 1000)
