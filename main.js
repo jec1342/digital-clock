bb220598c163d2cb2a73ea9792bd5eee7a746584
Jacob Collins <jacob.edward.collins@gmail.com>
2021-02-19 13:07:59 -0500

    Add style elements

---
+++ main.js
@@ -0,0 +1,29 @@
+setInterval(displayTime, 1000);
+
+function displayTime() {
+    let date = new Date() 
+    let hour = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+    let period = "AM";
+
+    if (hour>12) {
+        hour -= 12;
+        period = "PM";
+    }
+
+    if (hour < 12) {
+        period = "AM";
+    }
+
+hour = hour <10 ? "0" + hour : hour;
+mins = mins < 10 ? "0" + mins : mins;
+secs = secs < 10 ? "0" + secs : secs;
+
+let currentTime = hour + ":" + mins + ":" + secs + period
+
+document.getElementById("JacobsDigitalClock")
+    .innerHTML = currentTime
+}
+
+displayTime()
